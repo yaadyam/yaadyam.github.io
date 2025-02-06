dd197dcf1d112dab2a19b99b3de6a3fb2fbb5cc5
Matt Cuvanov <eonjugg@gmail.com>
2025-02-06 18:21:22 -0500

    added basic randomizer functionality (no pictures yet)

---
+++ wtd/rd.js
@@ -0,0 +1,33 @@
+/*
+    For ease of updating the unit stats, all the game data is stored locally on this server as ~/wtd/data.json
+    "game_version" is the version number the data is updated for 
+*/
+
+let gd // JSON object containing game data
+
+async function getData() {
+    const url = "./data.json"
+    const response = await fetch(url)
+    const json = await response.json()
+    gd = json
+    document.getElementById("gameversion").innerText = "for WTD " + gd.game_version
+}
+
+getData()
+
+function randomize() {
+    const units = gd["units"]
+    let chosenindices = []
+    for (let i = 0; i < 5; i++) {
+        let choice;
+        do {
+            choice = Math.floor(Math.random() * Object.keys(units).length )
+        } while (chosenindices.includes(choice))
+        chosenindices.push(choice)
+    }
+    for (let i = 1; i <= 5; i++) {
+        let chosenunit = Object.keys(units)[chosenindices[i-1]];
+        let pageslot = document.getElementById("unit" + i);
+        pageslot.lastElementChild.innerText = chosenunit.charAt(0).toUpperCase() + chosenunit.slice(1)
+    }
+}
