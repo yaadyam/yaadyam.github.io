27ee263dca8dc135633b963f4abae454e412a70d
eonjugg <eonjugg@gmail.com>
2025-02-21 20:55:06 -0500

    simplified html request

--- friendspace/look.js
+++ friendspace/look.js
@@ -3,17 +3,31 @@ function getinputuser() {
     console.log(uid)
 
     // https://create.roblox.com/docs/cloud/legacy/friends/v1#/Friends/get_v1_users__userId__friends
-    httpGetAsync(`https://corsproxy.io/?url=https://friends.roblox.com/v1/users/${uid}/friends`, (responseText) => {
-        console.log(responseText)
-        listfriends(responseText)
+    let friendsreq = new XMLHttpRequest()
+    friendsreq.addEventListener("load", (e) => {
+        console.log(friendsreq.response)
+        listfriends(friendsreq.response)
     })
+    friendsreq.open("GET", `https://corsproxy.io/?url=https://friends.roblox.com/v1/users/${uid}/friends`)
+    friendsreq.send()
+
+    /* // https://create.roblox.com/docs/cloud/legacy/users/v1#/Users/get_v1_users__userId_
+    httpGetAsync(`https://corsproxy.io/?url=https://users.roblox.com/v1/users/${uid}`, (responseText) => {
+        console.log(responseText)
+        let userinfo = JSON.parse(responseText)
+        document.getElementById("inputusername").innerText = `${userinfo.name}'s friends:`
+        
+    }) */
+
 }
 
 getinputuser()
 
-function listfriends(responseText) {
-    let friends = JSON.parse(responseText)
+function listfriends(response) {
+    let friends = JSON.parse(response)
     let table = document.getElementById("friendstable")
+    let tbody = table.querySelector("tbody")
+    tbody.innerHTML = ''
     friends.data.forEach(user => {
         let row = document.createElement("tr")
 
@@ -31,14 +45,4 @@ function listfriends(responseText) {
         table.querySelector("tbody").appendChild(row)
     });
     table.style.display = "table"
-}
-
-function httpGetAsync(theUrl, callback) {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() { 
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-            callback(xmlHttp.responseText)
-    }
-    xmlHttp.open("GET", theUrl, true) // true for asynchronous 
-    xmlHttp.send(null)
 }
